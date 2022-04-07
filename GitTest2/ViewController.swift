11d383c7af45899214a9f48b78c930aeda3837ea
omkar-z <omkarz@bipp.io>
2022-04-07 16:52:45 +0530

    Initial Commit

---
+++ GitTest2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitTest2
+//
+//  Created by Admin on 07/04/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
